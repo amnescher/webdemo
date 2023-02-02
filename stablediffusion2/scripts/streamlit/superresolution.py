394e1db99890068c6bd4144cde52ec66c9993c96
AmnEscherCloud <a.sabet@eschercloud.ai>
2023-02-02 14:09:10 +0000

    fix dockerfile

--- stablediffusion2/scripts/streamlit/superresolution.py
+++ stablediffusion2/scripts/streamlit/superresolution.py
@@ -28,7 +28,7 @@ def initialize_model(config, ckpt):
     config = OmegaConf.load(config)
     model = instantiate_from_config(config.model)
     model.load_state_dict(torch.load(ckpt)["state_dict"], strict=False)
-
+    model = torch.compile(model)
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     model = model.to(device)
     sampler = DDIMSampler(model)
