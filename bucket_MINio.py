56b7c87bcfecddbfdd192fa2e86a2127344cb875
AmnEscherCloud <a.sabet@eschercloud.ai>
2023-02-22 09:31:14 +0000

    result

--- bucket_MINio.py
+++ bucket_MINio.py
@@ -10,7 +10,7 @@ def main():
     # Create a client with the MinIO server playground, its access key
     # and secret key.
 
-    load_dotenv("./env_client.env")
+    load_dotenv("./env_server.env")
     access_key = os.environ.get('access_key')
     secret_key = os.environ.get('secret_key')
 
@@ -29,53 +29,49 @@ def main():
     else:
         print("Bucket 'modelweight' already exists")
 
-    # found = client.bucket_exists("configdata")
-    # if not found:
-    #     client.make_bucket("configdata")
-    # else:
-    #     print("Bucket 'config_data' already exists")
-    # client.fput_object(
-    #     "configdata", "storage/config.yaml", "storage/config.yaml",
-    # )
-    # # Upload '/home/user/Photos/asiaphotos.zip' as object name
-    # # 'asiaphotos-2015.zip' to bucket 'asiatrip'.
-    # #Write weights of stable diffusion version 1 
-    # # client.fput_object(
-    # #     "modelweight", "storage/model_weights/diff1/model_v1.ckpt", "storage/model_weights/diff1/model_v1.ckpt",
-    # # )
-    # # print(
-    # #     "'storage/model_weights/diff1/model_v1.ckpt' is successfully uploaded as "
-    # #     "object 'model_v1.ckpt' to bucket 'modelweight'."
-    # # )
-    # # # Write weights of stable diffusion version 1 
-    # # client.fput_object(
-    # #     "modelweight", "storage/model_weights/diff2/model_v2_768.ckpt", "storage/model_weights/diff2/model_v2_768.ckpt",
-    # # )
-    # # print(
-    # #     "'storage/model_weights/diff2/model_v2_768.ckpt' is successfully uploaded as "
-    # #     "object 'model_v2_768.ckpt' to bucket 'asiatrip'."
-    # # )
+    found = client.bucket_exists("configdata")
+    if not found:
+        client.make_bucket("configdata")
+    else:
+        print("Bucket 'config_data' already exists")
+    client.fput_object(
+        "configdata", "storage/config.yaml", "/home/ubuntu/dev/webapp/webdemo/storage/config.yaml",
+    )
+    # Upload '/home/user/Photos/asiaphotos.zip' as object name
+    # 'asiaphotos-2015.zip' to bucket 'asiatrip'.
+    #Write weights of stable diffusion version 1 
+    client.fput_object(
+        "modelweight", "storage/model_weights/diff1/model_v1.ckpt", "/home/ubuntu/dev/webapp/webdemo/storage/model_weights/diff1/model_v1.ckpt",
+    )
+    print(
+        "'storage/model_weights/diff1/model_v1.ckpt' is successfully uploaded as "
+        "object 'model_v1.ckpt' to bucket 'modelweight'."
+    )
+    # # Write weights of stable diffusion version 1 
+    client.fput_object(
+        "modelweight", "storage/model_weights/diff2/model_v2_768.ckpt", "/home/ubuntu/dev/webapp/webdemo/storage/model_weights/diff2/model_v2_768.ckpt",
+    )
+    print(
+        "'storage/model_weights/diff2/model_v2_768.ckpt' is successfully uploaded as "
+        "object 'model_v2_768.ckpt' to bucket 'asiatrip'."
+    )
 
-    # client.fput_object(
-    #     "modelweight", "storage/model_weights/diff2/512-inpainting-ema.ckpt", "storage/model_weights/diff2/512-inpainting-ema.ckpt",
-    # )
-    # print(
-    #     "'storage/model_weights/diff2/512-inpainting-ema.ckpt' is successfully uploaded as "
-    #     "object 'model_v2_768.ckpt' to bucket 'asiatrip'."
-    # )
+    client.fput_object(
+        "modelweight", "storage/model_weights/diff2/512-inpainting-ema.ckpt", "/home/ubuntu/dev/webapp/webdemo/storage/model_weights/diff2/512-inpainting-ema.ckpt",
+    )
+    print(
+        "'storage/model_weights/diff2/512-inpainting-ema.ckpt' is successfully uploaded as "
+        "object 'model_v2_768.ckpt' to bucket 'asiatrip'."
+    )
 
 
-    # # client.fput_object(
-    # #     "modelweight", "storage/model_weights/diff2/x4-upscaler-ema.ckpt", "storage/model_weights/diff2/x4-upscaler-ema.ckpt",
-    # # )
-    # # print(
-    # #     "'storage/model_weights/diff2/x4-upscaler-ema.ckpt' is successfully uploaded as "
-    # #     "object 'model_v2_768.ckpt' to bucket 'asiatrip'."
-    # # )
-
-    # # client.fput_object(
-    # #     "config_data", "storage/config.yaml", "storage/config.yaml",
-    # # )
+    client.fput_object(
+        "modelweight", "storage/model_weights/diff2/x4-upscaler-ema.ckpt", "/home/ubuntu/dev/webapp/webdemo/storage/model_weights/diff2/x4-upscaler-ema.ckpt",
+    )
+    print(
+        "'storage/model_weights/diff2/x4-upscaler-ema.ckpt' is successfully uploaded as "
+        "object 'model_v2_768.ckpt' to bucket 'asiatrip'."
+    )
 
     client.fput_object(
         "modelweight", "storage/model_weights/GFPGAN/GFPGANv1.3.pth", "/home/ubuntu/dev/webapp/webdemo/storage/model_weights/GFPGAN/GFPGANv1.3.pth",
